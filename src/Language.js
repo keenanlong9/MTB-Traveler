0f762185c3ecc0c36619317fc21e40921d63bb0f
Keenan Long <keenanlong16@gmail.com>
2024-05-05 13:56:23 -0400

    Adding translate logic

---
+++ src/Language.js
@@ -0,0 +1,33 @@
+import { CognitoIdentityClient } from "@aws-sdk/client-cognito-identity";
+import { fromCognitoIdentityPool } from "@aws-sdk/credential-provider-cognito-identity";
+import { TranslateClient, TranslateTextCommand } from "@aws-sdk/client-translate";
+
+const translateText = async () => {
+    const inputText = document.getElementById("input_text").value;
+    const outputText = document.getElementById("output_text");
+
+    const translateClient = new TranslateClient({
+        region: "us-east-1",
+        credentials: fromCognitoIdentityPool({
+            client: new CognitoIdentityClient({region: "us-east-1"}),
+            identityPoolId: "us-east-1:891d8d0f-1caf-4b14-81f7-71114388deb6"
+        }),
+    });
+
+    const translateParams = {
+        Text: inputText,
+        SourceLanguageCode: "auto",
+        TargetLanguageCode: "fr"
+    };
+
+    const translateTextCommand = new TranslateTextCommand(translateParams);
+    try{
+        const data = await translateClient.send(translateTextCommand);
+        outputText.textContent = data.TranslatedText;
+    } catch (error) {
+        console.error("Error", error);
+        outputText.textContent = "Error";
+    }
+}
+
+document.getElementById("translate_btn").addEventListener("click", translateText);
