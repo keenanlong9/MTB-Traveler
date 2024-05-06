8f2a380fe41ae16bd62645e0eebc1b2e10fcc8d5
Keenan Long <keenanlong16@gmail.com>
2024-05-05 22:34:14 -0400

    Added AWS polly output

--- src/Language.js
+++ src/Language.js
@@ -1,6 +1,8 @@
 import { CognitoIdentityClient } from "@aws-sdk/client-cognito-identity";
 import { fromCognitoIdentityPool } from "@aws-sdk/credential-provider-cognito-identity";
 import { TranslateClient, TranslateTextCommand } from "@aws-sdk/client-translate";
+import { LanguageCode, Polly } from "@aws-sdk/client-polly";
+import { getSynthesizeSpeechUrl } from "@aws-sdk/polly-request-presigner";
 
 const translateText = async () => {
     const inputText = document.getElementById("input_text").value;
@@ -23,10 +25,44 @@ const translateText = async () => {
     try{
         const data = await translateClient.send(translateTextCommand);
         outputText.value = data.TranslatedText;
+        audioTranslateText();
     } catch (error) {
         console.error("Error", error);
         outputText.value = "Error";
     }
-}
+};
+
+const audioTranslateText = async () => {
+    const pollyClient = new Polly({
+        region: "us-east-1",
+        credentials: fromCognitoIdentityPool({
+            client: new CognitoIdentityClient({region: "us-east-1"}),
+            identityPoolId: "us-east-1:891d8d0f-1caf-4b14-81f7-71114388deb6"
+        }),
+    });
+
+    const speechParams = {
+        OutputFormat: "mp3",
+        Text: "",
+        TextType: "text",
+        VoiceId: "Matthew",
+        LanguageCode: "fr-FR"
+      };
+
+
+      speechParams.Text = document.getElementById("output_text").value;
+      try {
+        let url = await getSynthesizeSpeechUrl({
+          client: pollyClient,
+          params: speechParams,
+        });
+        console.log(url);
+        // Load the URL of the voice recording into the browser
+        document.getElementById("translate_audio_src").src = url;
+        document.getElementById("translate_audio").load();
+      } catch (err) {
+        console.log("Error", err);
+      }
+};
 
 window.translateText = translateText;
