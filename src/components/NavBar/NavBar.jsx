4b3a1ec412b5f525e97672028f9beec33e94e224
Keenan Long <keenanlong16@gmail.com>
2025-06-10 21:50:07 -0400

    Added toggling sign in/out button with basic sign in form

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,30 @@
 import { Link } from "react-router-dom"
-import { useState } from "react"
-import { signOut } from '@aws-amplify/auth'
+import { useState, useEffect } from "react"
+import { signOut, signIn, getCurrentUser } from '@aws-amplify/auth'
 import "./NavBar.css"
 
 export default function NavBar () {
   const [menuActive, setMenuActive] = useState(false);
+  const [user, setUser] = useState(null);
+  const [form, setForm] = useState({ username: '', password: '' });
+
+const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  // Fetch user on component mount
+  useEffect(() => {
+    checkUser();
+  }, []);
+
+  const checkUser = async () => {
+    try {
+      const currentUser = await getCurrentUser();
+      setUser(currentUser);
+    } catch (error) {
+      setUser(null);
+    }
+  };
 
   const handleMenuToggle = () => {
     setMenuActive(!menuActive);
@@ -13,12 +33,25 @@ export default function NavBar () {
   const handleSignOut = async () => {
     try {
       await signOut();
+      setUser(null);
       console.log('User signed out');
     } catch (error) {
       console.log('Error signing out: ', error);
     }
   };
 
+  const handleSignIn = async () => {
+    try {
+      const signedInUser = await signIn({
+        username: form.username,
+        password: form.password,
+      });
+      setUser(signedInUser);
+      console.log('User signed in');
+    } catch (error) {
+      console.error('Sign-in error:', error);
+    }
+  };
 
     return (  
       <nav className="navbar">
@@ -40,7 +73,24 @@ export default function NavBar () {
               <Link to="/language" className="navbar_links">Language Translator</Link>
             </li>
             <li className="navbar_item-btn">
-                <button className="navbar_item-btn" onClick={handleSignOut}>Sign Out</button>
+              {user ? 
+              (<button className="navbar_item-btn" onClick={handleSignOut}>Sign Out</button> ) 
+              : (<div>
+                <input
+                  name="username"
+                  placeholder="Username"
+                  value={form.username}
+                  onChange={handleChange}
+                />
+                <input
+                  name="password"
+                  type="password"
+                  placeholder="Password"
+                  value={form.password}
+                  onChange={handleChange}
+                />
+              <button className="navbar_item-btn" onClick={handleSignIn}>Sign In</button>
+              </div>)}
             </li>
           </ul>
         </div>
